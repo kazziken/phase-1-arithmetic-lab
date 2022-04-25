62eab78256d5841d947479bc926b8e3917233f94
Kenneth Mei <meikenneth321@gmail.com>
2022-04-25 17:49:52 -0400

    commit tree a105d011a964a3dc5b4c442f153502a50ed3de43

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+multiply;
 
-// Write your code here
+random = Math.floor(Math.random() * 10) + 1;
+random;
+
+let num3 = 9
+let num4 = 5
+let mod = num3/num4
+mod;
+
+max = Math.max(1, 2, 3, 4, 20);
+max;
